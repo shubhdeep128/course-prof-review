97e0350530a16b7b7d58e2de56d5726d6bae6716
DesuSreeHarsha <33808905+DesuSreeHarsha@users.noreply.github.com>
2020-02-16 18:47:08 +0530

    Add files via upload
    Made rest of the schemas

--- models/Professor.js
+++ models/Professor.js
@@ -1,3 +1,25 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+var Professor = new Schema({
+    Name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    Description: {
+        type: String,
+        required:true
+    },
+    Course: {
+        type: [Object]
+    },
+    Reviews: {
+        type: [Object]
+    },
+    Relevent_tags: {
+        type: [String]
+    },
+});
+
+module.exports = mongoose.model('Professor', Professor);
