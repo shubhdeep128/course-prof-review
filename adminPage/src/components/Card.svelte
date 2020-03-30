1c0252319b12d0652aa1f9071631aad1475973e5
Subhash Reddy <subhash8111999@gmail.com>
2020-03-30 17:14:06 +0530

    Create Card.svelte

---
+++ adminPage/src/components/Card.svelte
@@ -0,0 +1,26 @@
+<script>
+export let title=""
+</script>
+
+<div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      {title}
+    </p>
+  </header>
+  <div class="card-content">
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+  <footer class="card-footer">
+    <slot name="footer" />
+  </footer>
+</div>
+
+
+<style>
+.card {
+  margin: 10px
+}
+</style>
