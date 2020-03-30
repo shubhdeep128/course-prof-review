2cc19150fb552f9d18d20ba05c0f6cfb510f8cac
Subhash Reddy <subhash8111999@gmail.com>
2020-03-30 18:07:13 +0530

    Delete http.js

--- adminPage/src/http.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const http = axios.create({
-    baseURL: "https://northwind.now.sh/api",
-    timeout: 100000,
-    headers: { 'Content-Type': 'application/json' }
-  })
-
-export default http
