ed9be7313786e0e404a7bd18094ebb17c40ae77d
Subhash Reddy <subhash8111999@gmail.com>
2020-03-30 17:14:23 +0530

    Create about.svelte

---
+++ adminPage/src/routes/about.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<script>
+import  Card  from '../components/Card.svelte'
+</script>
+
+<Card title="About">
+	<div>
+		This is the 'about' page. There's not much here.
+	</div>
+</Card>
