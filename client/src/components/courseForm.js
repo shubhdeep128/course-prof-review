4d75196c24345d883dd7b9730126acdc7bbd30e2
Subhash Reddy <subhash8111999@gmail.com>
2020-04-01 12:44:52 +0530

    Update courseForm.js

--- client/src/components/courseForm.js
+++ client/src/components/courseForm.js
@@ -38,34 +38,6 @@ class CourseForm extends Component {
       </div>
     );
   }
-
-  addCourse(e){
-    e.preventDefault()
-    var desc = this.refs.desc.value;
-    var title = this.refs.title.value;
-    var year = Number(this.refs.year.value);
-    var tags = this.refs.cast.value.split(',');
-    cast = cast.map(item => item.trim());
-    console.log(cast);
-    if(Number.isNaN(year)||year < 1888){
-      alert('Year must be a number greater than 1888. The first movie in the world was released in 1888');
-    }
-    else{
-      API.post('/api/course/add', {
-        Name : Name,
-        Description: Description,
-        Professor_history: [],
-        Reviews: [],
-        Relevant_tags: [],
-        Average_grade: 0
-      }).then(function (response) {
-        alert("Course added successfully!");
-        window.location = '/course/add';
-      }).catch(function (error) {
-        console.log(error);
-      });
-	}
-  }
 }
 
 export default CourseForm;
