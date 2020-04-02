eb32a5d016857f746d2d78729af0622c379de603
Subhash Reddy <subhash8111999@gmail.com>
2020-04-02 09:40:53 +0530

    Update CourseController.js

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -74,11 +74,8 @@ router.get('/list', (req, res) => {
 function handleValidationError(err, body) {
     for (field in err.errors) {
         switch (err.errors[field].path) {
-            case 'fullName':
-                body['fullNameError'] = err.errors[field].message;
-                break;
-            case 'email':
-                body['emailError'] = err.errors[field].message;
+            case 'Name':
+                body['NameError'] = err.errors[field].message;
                 break;
             default:
                 break;
