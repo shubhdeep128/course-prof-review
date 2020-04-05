225b60908d9860c405e01f6d3e8b7013ac6c7aef
Subhash Reddy <subhash8111999@gmail.com>
2020-04-06 02:11:56 +0530

    Create mongodb.js

---
+++ models/mongodb.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true}, (err) => {
+if (!err) {
+console.log('Successfully Established Connection with MongoDB')
+}
+else {
+console.log('Failed to Establish Connection with MongoDB with Error: '+ err)
+}
+});
+ 
+//Connecting Node and MongoDB
+require('./Course');
+require('./Professor');
