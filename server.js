1d296ca7bfb9e11bebec65656118c8a63a369017
Subhash Reddy <subhash8111999@gmail.com>
2020-04-06 02:11:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-require('./models/db.course');
-require('./models/db.professor');
-
+require('./models/mongodb');
+ 
+//Import the necessary packages
 const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const bodyparser = require('body-parser');
-
-const CourseController = require('./controllers/CourseController');
-const ProfessorController = require('./controllers/ProfessorController');
-
 var app = express();
+const path = require('path');
+const exphb = require('express-handlebars');
+const bodyparser = require('body-parser');
+ 
+const courseController = require('./controllers/courseController');
+const professorController = require('./controllers/professorController');
+ 
 app.use(bodyparser.urlencoded({
-    extended: true
+extended: true
 }));
-app.use(bodyparser.json());
-app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
-app.set('view engine', 'hbs');
-
-app.listen(3000, () => {
-    console.log('Express server started at port : 3000');
+ 
+//Create a welcome message and direct them to the main page
+app.get('/', (req, res) => {
+res.send(' <h2 style="font-family: Malgun Gothic; color: midnightblue ">Admin Page </h2>Click Here to go to <b> <a href="/course">Course Page</a> </b><b> <a href="/professor">Professor Page</a> </b>');
 });
-
-app.use('/course', CourseController);
-app.use('/professor', ProfessorController)
+app.use(bodyparser.json());
+ 
+//Configuring Express middleware for the handlebars
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphb({ extname: 'hbs', defaultLayout: 'mainLayout', layoutDir: __dirname + 'views/layouts/' }));
+app.set('view engine', 'hbs');
+ 
+//Establish the server connection
+//PORT ENVIRONMENT VARIABLE
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
+ 
+//Set the Controller path which will be responding the user actions
+app.use('/course', courseController);
+app.use('/professor', professorController);
