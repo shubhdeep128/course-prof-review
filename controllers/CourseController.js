8d541ca494100e423c185ff19f70c8bae54292bd
Subhash Reddy <subhash8111999@gmail.com>
2020-04-06 02:11:45 +0530

    Update CourseController.js

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -1,106 +1,106 @@
+//Import the dependencies
 const express = require('express');
-var router = express.Router();
 const mongoose = require('mongoose');
+//Creating a Router
+var router = express.Router();
+//Link
 const Course = mongoose.model('Course');
-
-router.get('/', (req, res) => {
-    res.render("operations/addOrEdit", {
-        viewTitle: "Insert Course"
-    });
+ 
+//Router Controller for READ request
+router.get('/',(req, res) => {
+res.render("course/courseAddEdit", {
+viewTitle: "Insert a New Course"
 });
-
-router.post('/', (req, res) => {
-    if (req.body._id == '')
-        insertRecord(req, res);
-        else
-        updateRecord(req, res);
 });
-
-
-function insertRecord(req, res) {
-    var Course = new Course();
-    Course.Name = req.body.Name;
-    Course.Description = req.body.Description;
-    Course.Average_grade = req.body.Average_grade;
-    Course.save((err, doc) => {
-        if (!err)
-            res.redirect('operations/list');
-        else {
-            if (err.name == 'ValidationError') {
-                handleValidationError(err, req.body);
-                res.render("operations/addOrEdit", {
-                    viewTitle: "Insert Course",
-                    Course: req.body
-                });
-            }
-            else
-                console.log('Error during record insertion : ' + err);
-        }
-    });
+ 
+//Router Controller for UPDATE request
+router.post('/', (req,res) => {
+if (req.body._id == '')
+insertIntoMongoDB(req, res);
+else
+updateIntoMongoDB(req, res);
+});
+ 
+//Creating function to insert data into MongoDB
+function insertIntoMongoDB(req,res) {
+var course = new Course();
+course.Name = req.body.Name;
+course.Description = req.body.Description;
+course.Average_grade = req.body.Average_grade;
+course.save((err, doc) => {
+if (!err)
+res.redirect('course/list');
+else
+console.log('Error during record insertion : ' + err);
+});
 }
-
-function updateRecord(req, res) {
-    Course.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
-        if (!err) { res.redirect('operations/list'); }
-        else {
-            if (err.name == 'ValidationError') {
-                handleValidationError(err, req.body);
-                res.render("operations/addOrEdit", {
-                    viewTitle: 'Update Course',
-                    Course: req.body
-                });
-            }
-            else
-                console.log('Error during record update : ' + err);
-        }
-    });
-}
-
-
-router.get('/list', (req, res) => {
-    Course.find((err, docs) => {
-        if (!err) {
-            res.render("operations/list", {
-                list: docs
-            });
-        }
-        else {
-            console.log('Error in retrieving Course list :' + err);
-        }
-    });
+ 
+//Creating a function to update data in MongoDB
+function updateIntoMongoDB(req, res) {
+Course.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+if (!err) { res.redirect('course/list'); }
+else {
+if (err.name == 'ValidationError') {
+handleValidationError(err, req.body);
+res.render("course/courseAddEdit", {
+//Retaining value to be displayed in the child view
+viewTitle: 'Update Course Details',
+employee: req.body
 });
-
-
+}
+else
+console.log('Error during updating the record: ' + err);
+}
+});
+}
+ 
+//Router to retrieve the complete list of available courses
+router.get('/list', (req,res) => {
+Course.find((err, docs) => {
+if(!err){
+res.render("course/list", {
+list: docs
+});
+}
+else {
+console.log('Failed to retrieve the Course List: '+ err);
+}
+});
+});
+ 
+//Creating a function to implement input validations
 function handleValidationError(err, body) {
-    for (field in err.errors) {
-        switch (err.errors[field].path) {
-            case 'Name':
-                body['NameError'] = err.errors[field].message;
-                break;
-            default:
-                break;
-        }
-    }
+for (field in err.errors) {
+switch (err.errors[field].path) {
+case 'Name':
+body['NameError'] = err.errors[field].message;
+break;
+default:
+break;
 }
-
+}
+}
+ 
+//Router to update a course using it's ID
 router.get('/:id', (req, res) => {
-    Course.findById(req.params.id, (err, doc) => {
-        if (!err) {
-            res.render("operations/addOrEdit", {
-                viewTitle: "Update Course",
-                Course: doc
-            });
-        }
-    });
+Course.findById(req.params.id, (err, doc) => {
+if (!err) {
+res.render("course/courseAddEdit", {
+viewTitle: "Update Course Details",
+course: doc
 });
-
+}
+});
+});
+ 
+//Router Controller for DELETE request
 router.get('/delete/:id', (req, res) => {
-    Course.findByIdAndRemove(req.params.id, (err, doc) => {
-        if (!err) {
-            res.redirect('/operations/list');
-        }
-        else { console.log('Error in Course delete :' + err); }
-    });
+Course.findByIdAndRemove(req.params.id, (err, doc) => {
+if (!err) {
+res.redirect('/course/list');
+}
+else { console.log('Failed to Delete Course Details: ' + err); }
 });
-
+});
+ 
 module.exports = router;
