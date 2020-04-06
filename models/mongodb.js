48d80c20daf929843edc1c1514d38123766cbe22
Subhash Reddy <subhash8111999@gmail.com>
2020-04-06 09:57:56 +0530

    Update mongodb.js

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
  
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true}, (err) => {
+mongoose.connect('mongodb+srv://shubh_162:hx3G011451pnfuE4@cluster0-9qlu7.mongodb.net/test', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    }, (err) => {
 if (!err) {
 console.log('Successfully Established Connection with MongoDB')
 }
