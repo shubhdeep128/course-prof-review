bf3099bf3b7a1c40aea3b6e8941d18d2b3f5d915
nibaplis <shubhdeep41280@gmail.com>
2020-06-10 15:16:13 +0530

    Modified dashboard

--- client/src/shards-dashboard/src/views/BlogOverview.js
+++ client/src/shards-dashboard/src/views/BlogOverview.js
@@ -6,7 +6,8 @@ import SmallStats from "./../components/common/SmallStats";
 import UsersOverview from "./../components/blog/UsersOverview";
 import UsersByDevice from "./../components/blog/UsersByDevice";
 import API from "../../../utils/API";
-
+import axios from 'axios';
+import Loading from '../../../components/Loading'
 class BlogOverview extends Component {
 
   constructor(props) {
@@ -15,52 +16,113 @@ class BlogOverview extends Component {
       courseData : [],
       profData : [],
       reviewData : [],
-      newUserData : [],
-      userData : []
+      visitData : [],
+      userData : [],
+      courseCount : 0,
+      visitCount : 0,
+      profCount : 0,
+      reviewCount : 0,
+      userCount : 0,
+      loadStatus : false,
     }
   }
 
   
 
   componentDidMount(){
-    API.get(`/api/course/count`)
-      .then((response) => {
-        console.log(response.data['courses']);
-        this.setState({courseData : { count : response.data['courses']}})
-      })
-    API.get(`/api/prof/count`)
-      .then((response) => {
-        console.log(response.data['Professors']);
-        this.setState({profData : { count : response.data['Professors']}})
+    // API.get(`/api/course/count`)
+    //   .then((response) => {
+    //     // console.log(response.data['courses']);
+    //     this.setState({ courseCount : response.data['courses']})
+    //   })
+    // API.get(`/api/prof/count`)
+    //   .then((response) => {
+    //     // console.log(response.data['Professors']);
+    //     this.setState({profCount : response.data['Professors']})
 
-      })
-    API.get(`/api/review/count`)
-      .then((response) => {
-        console.log(response.data['Reviews']);
-        this.setState({reviewData : { count : response.data['Reviews']}})
+    //   })
+    // API.get(`/api/review/count`)
+    //   .then((response) => {
+    //     // console.log(response.data['Reviews']);
+    //     this.setState({reviewCount : response.data['Reviews']})
 
-      })
-    API.get(`/api/user/count`)
-      .then((response) => {
-        console.log(response.data['users']);
-        this.setState({userData : { count : response.data['users']}})
+    //   })
+    // API.get(`/api/user/count`)
+    //   .then((response) => {
+    //     // console.log(response.data['users']);
+    //     this.setState({userCount : response.data['users']})
 
-      })
-    API.get(`/api/stats/users?days=7`)
-      .then((response) => {
-        console.log("hello")
-        console.log(response);
+    //   })
 
-        this.setState({newUserData : {count: response.data.total}})
-      })
+    axios.all([
+      axios.get(`/api/course/count`),
+      axios.get(`/api/prof/count`),
+      axios.get(`/api/review/count`),
+      axios.get(`/api/user/count`),
+      axios.get(`/api/stats/views`),
+      axios.get(`/api/stats/course?days=30`),
+      axios.get(`/api/stats/prof?days=30`),
+      axios.get(`/api/stats/review?days=30`),
+      axios.get(`/api/stats/users?days=30`),
+      axios.get(`/api/stats/visits?days=30`),
+
+    ]).then(responseArr => {
+      this.setState({
+        courseCount: responseArr[0].data["courses"],
+        profCount: responseArr[1].data["Professors"],
+        reviewCount: responseArr[2].data["Reviews"],
+        userCount: responseArr[3].data["users"],
+        visitCount: responseArr[4].data["views"],
+        courseData: responseArr[5].data.data,
+        profData: responseArr[6].data.data,
+        reviewData: responseArr[7].data.data,
+        userData: responseArr[8].data.data,
+        visitData: responseArr[9].data.data,
+      }, () => {this.setState({loadStatus:true})})
+
+      console.log(responseArr)
+    }).catch(function (error) {
+      console.log("ERROR LOADING DATA");
+      console.log(error);
+    });
+      
+    // API.get(`/api/stats/visits?days=30`)
+    //   .then((response) => {
+    //     console.log("hello")
+    //     console.log(response);
+    //     const { dataArr, count } = response.data
+    //     this.setState({visitData : dataArr, visitCount : count}, () => {console.log(this.state.visitData)})
+    //   })
+    // API.get(`/api/stats/course?days=30`)
+    //   .then((response) => {
+    //     // console.log(response);
+    //     var tempData = this.state.courseData;
+    //     tempData["data"] = response.data;
+    //     this.setState({courseData : tempData})
+    //   })
+    //   API.get(`/api/stats/prof?days=30`)
+    //   .then((response) => {
+    //     // console.log(response);
+    //     var tempData = this.state.profData;
+    //     tempData["data"] = response.data;
+    //     this.setState({profData : tempData})
+    //   })
+    //   API.get(`/api/stats/review?days=30`)
+    //   .then((response) => {
+    //     // console.log(response);
+    //     var tempData = this.state.reviewData;
+    //     tempData["data"] = response.data;
+    //     this.setState({reviewData : tempData})
+    //   })
   }
 
   render() {
-    const smallStats = [
+    // console.log(this.state)
+    var smallStats = [
       {
         label: "Courses",
-        value: this.state.courseData.count || "Error",
-        percentage: "4.7%",
+        value: this.state.courseCount || "Error",
+        percentage: "",
         increase: true,
         chartLabels: [null, null, null, null, null, null, null],
         attrs: { md: "6", sm: "6" },
@@ -71,14 +133,14 @@ class BlogOverview extends Component {
             borderWidth: 1.5,
             backgroundColor: "rgba(0, 184, 216, 0.1)",
             borderColor: "rgb(0, 184, 216)",
-            data: [1, 2, 1, 3, 5, 4, 7]
+            data: this.state.courseData
           }
         ]
       },
       {
         label: "Professors",
-        value: this.state.profData.count || "Error",
-        percentage: "12.4",
+        value: this.state.profCount || "Error",
+        percentage: "",
         increase: true,
         chartLabels: [null, null, null, null, null, null, null],
         attrs: { md: "6", sm: "6" },
@@ -89,14 +151,14 @@ class BlogOverview extends Component {
             borderWidth: 1.5,
             backgroundColor: "rgba(23,198,113,0.1)",
             borderColor: "rgb(23,198,113)",
-            data: [1, 2, 3, 3, 3, 4, 4]
+            data: this.state.profData
           }
         ]
       },
       {
         label: "Reviews",
-        value: this.state.reviewData.count || "Error",
-        percentage: "3.8%",
+        value: this.state.reviewCount || "Error",
+        percentage: "",
         increase: false,
         decrease: true,
         chartLabels: [null, null, null, null, null, null, null],
@@ -108,14 +170,14 @@ class BlogOverview extends Component {
             borderWidth: 1.5,
             backgroundColor: "rgba(255,180,0,0.1)",
             borderColor: "rgb(255,180,0)",
-            data: [2, 3, 3, 3, 4, 3, 3]
+            data: this.state.reviewData
           }
         ]
       },
       {
-        label: "New Users",
-        value: this.state.newUserData.count || "Error",
-        percentage: "2.71%",
+        label: "Site Visits",
+        value: this.state.visitCount || "Error",
+        percentage: "",
         increase: false,
         decrease: true,
         chartLabels: [null, null, null, null, null, null, null],
@@ -127,14 +189,14 @@ class BlogOverview extends Component {
             borderWidth: 1.5,
             backgroundColor: "rgba(255,65,105,0.1)",
             borderColor: "rgb(255,65,105)",
-            data: [1, 7, 1, 3, 1, 4, 8]
+            data: this.state.visitData 
           }
         ]
       },
       {
         label: "Total Users",
-        value: this.state.userData.count || "Error",
-        percentage: "2.4%",
+        value: this.state.userCount || "Error",
+        percentage: "",
         increase: false,
         decrease: true,
         chartLabels: [null, null, null, null, null, null, null],
@@ -146,7 +208,7 @@ class BlogOverview extends Component {
             borderWidth: 1.5,
             backgroundColor: "rgb(0,123,255,0.1)",
             borderColor: "rgb(0,123,255)",
-            data: [3, 2, 3, 2, 4, 5, 4]
+            data: this.state.userData
           }
         ]
       }
@@ -167,6 +229,7 @@ class BlogOverview extends Component {
         <Row>
           {smallStats.map((stats, idx) => (
             <Col className="col-lg mb-4" key={idx} {...stats.attrs}>
+              {this.state.loadStatus?
               <SmallStats
                 id={`small-stats-${idx}`}
                 variation="1"
@@ -177,7 +240,9 @@ class BlogOverview extends Component {
                 percentage={stats.percentage}
                 increase={stats.increase}
                 decrease={stats.decrease}
-              />
+              />:
+              <Loading type = {"spin"}/>
+              }
             </Col>
           ))}
         </Row>
--- routes/api/stats.js
+++ routes/api/stats.js
@@ -1,5 +1,7 @@
 var Course = require('../../models/Course')
 var User = require("../../models/User.js");
+var Prof = require('../../models/Professor')
+var Review = require('../../models/Review')
 var {PageView} = require("../../models/PageView.js");
 var utils = require("./utils.js");
 const express = require("express");
@@ -11,8 +13,6 @@ router = express.Router();
   });
 
   router.get("/course", (req,res)=>{
-	// how many movies were added over the last few days?
-	console.log("hrer")
 	var days = req.query.days;
 	console.log(days);
 	if(days == undefined) days = 6;
@@ -51,8 +51,85 @@ router = express.Router();
 	});
   });
 
+
+  router.get("/prof", (req,res)=>{
+	var days = req.query.days;
+	console.log(days);
+	if(days == undefined) days = 6;
+	console.log("[STATS: Prof] Returning last", days, "days of data");
+	var now = new Date();
+	var old_time = new Date(now.getTime()  - (days * 24 * 60 * 60 * 1000));
+	console.log(now,old_time)
+	var pipeline = [
+	    {	
+	    	"$match": {
+				"created_at": {
+					"$gte": old_time,
+					"$lte": now
+				}    		
+	    	}
+	    },
+	    {
+	        "$group": {
+	            "_id": {
+	            	"year":  { "$year": "$created_at"  },
+	            	"month": { "$month": "$created_at" },
+	            	"day":   { "$dayOfMonth": "$created_at" }
+	            },
+	            "count": { "$sum": 1 }
+	        }
+	    }
+	];
+	Prof.aggregate(pipeline, function (err, result){
+	    if (err) res.status(500).send({message:"Internal server error."});
+		result.sort(utils.compare_dates);
+		var daywise_data = [];
+		for(var i=0;i<result.length;i++){
+			daywise_data.push(result[i].count);
+		}
+	    res.status(200).send({data: daywise_data});
+	});
+  });
+  router.get("/review", (req,res)=>{
+	var days = req.query.days;
+	console.log(days);
+	if(days == undefined) days = 6;
+	console.log("[STATS: Reviews] Returning last", days, "days of data");
+	var now = new Date();
+	var old_time = new Date(now.getTime()  - (days * 24 * 60 * 60 * 1000));
+	console.log(now,old_time)
+	var pipeline = [
+	    {	
+	    	"$match": {
+				"created_at": {
+					"$gte": old_time,
+					"$lte": now
+				}    		
+	    	}
+	    },
+	    {
+	        "$group": {
+	            "_id": {
+	            	"year":  { "$year": "$created_at"  },
+	            	"month": { "$month": "$created_at" },
+	            	"day":   { "$dayOfMonth": "$created_at" }
+	            },
+	            "count": { "$sum": 1 }
+	        }
+	    }
+	];
+	Review.aggregate(pipeline, function (err, result){
+	    if (err) res.status(500).send({message:"Internal server error."});
+		result.sort(utils.compare_dates);
+		var daywise_data = [];
+		for(var i=0;i<result.length;i++){
+			daywise_data.push(result[i].count);
+		}
+	    res.status(200).send({data: daywise_data});
+	});
+  });
+
   router.get("/users", (req,res)=>{
-	// how many movies were added over the last few days?
 	var days = req.query.days;
 	if(days == undefined) days = 6;
 	console.log("[STATS: Users] Returning last", days, "days of data");
@@ -88,6 +165,51 @@ router = express.Router();
 			total = total+result[i].count
 		}
 	    res.status(200).send({data: daywise_data, total: total});
+	});
+  });
+
+  router.get("/visits", (req,res)=>{
+	// how many movies were added over the last few days?
+	var days = req.query.days;
+	console.log(days);
+	if(days == undefined) days = 6;
+	console.log("[STATS: SiteVisists] Returning last", days, "days of data");
+	var now = new Date();
+	var old_time = new Date(now.getTime()  - (days * 24 * 60 * 60 * 1000));
+	console.log(now,old_time)
+	var pipeline = [
+	    {	
+	    	"$match": {
+				"created_at": {
+					"$gte": old_time,
+					"$lte": now
+				}    		
+	    	}
+	    },
+	    {
+	        "$group": {
+	            "_id": {
+	            	"year":  { "$year": "$created_at"  },
+	            	"month": { "$month": "$created_at" },
+	            	"day":   { "$dayOfMonth": "$created_at" }
+	            },
+	            "count": { "$sum": 1 }
+	        }
+	    }
+	];
+	PageView.aggregate(pipeline, function (err, result){
+	    if (err) res.status(500).send({message:"Internal server error."});
+		result.sort(utils.compare_dates);
+		var daywise_data = [];
+		for(var i=0;i<result.length;i++){
+			daywise_data.push(result[i].count);
+		}
+		var total = 0;
+		for(var i=0;i<result.length;i++){
+			daywise_data.push(result[i].count);
+			total = total+result[i].count
+		}
+	    res.status(200).send({data: daywise_data, count : total});
 	});
   });
 
